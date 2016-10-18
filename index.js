afe930f4e9b18298030485fe531239dbe0efc957
Thomas Baron <thomasjbaron@hotmail.com>
2016-10-18 16:41:58 +0900

    Updated to work with or without browserify.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-module.exports = {
+var dom = {
 	'get': function(id) {
 		if (arguments.length === 1) {
 			return document.getElementById(id);
@@ -77,3 +77,5 @@ module.exports = {
 		return last;
 	}
 }
+
+if (module && module.exports) module.exports = dom;
